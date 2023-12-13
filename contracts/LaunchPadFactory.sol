7ffb762011bb34f8fdd7f4262cee87dfd1ee4d96
midesofek <midesofek@gmail.com>
2023-12-13 19:29:25 +0100

    integrated SFS Register function

--- contracts/LaunchPadFactory.sol
+++ contracts/LaunchPadFactory.sol
@@ -3,11 +3,23 @@ pragma solidity ^0.8.0;
 
 import "./LaunchPad.sol";
 
+contract Register {
+    function register(address _recipient) public returns (uint256 tokenId) {}
+}
+
 contract DeployFactory {
     mapping(uint256 => address) public createdLaunchPools;
     uint256 public poolCount;
 
     event LaunchPoolCreated(address indexed launchPoolAddress);
+
+    constructor() {
+        // sfs Mainnet Contract= 0x8680CEaBcb9b56913c519c069Add6Bc3494B7020
+        Register sfsContract = Register(
+            0xBBd707815a7F7eb6897C7686274AFabd7B579Ff6
+        ); // This address is the testnet address of the SFS contract
+        sfsContract.register(msg.sender); //Registers this contract and assigns the NFT to the owner of this contract
+    }
 
     function deploy(
         address _tokenAddress,
