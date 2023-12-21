15792343a3bdc11a1f55679ecf9ddc327412ccdf
midesofek <midesofek@gmail.com>
2023-12-21 14:29:38 +0100

    updated contract factory details

--- scripts/deploy-scripts/deployLaunchPadFactory.js
+++ scripts/deploy-scripts/deployLaunchPadFactory.js
@@ -2,7 +2,7 @@ const hre = require("hardhat");
 
 async function main() {
   console.log("Deploying Factory contract....");
-  const factory = await hre.ethers.deployContract("LaunchPadFactoryNoSFS", []);
+  const factory = await hre.ethers.deployContract("LaunchPadFactory", []);
   await factory.deployed();
 
   console.log(`Factory contract deployed to: \nAddress: ${factory.address}`);
